c34e34d4459065385615ca8ea9b3edb5f159a2ab
sbc14400 <sbc14400@gmail.com>
2017-06-10 13:59:03 +0900

    code modified in PyCharm

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,12 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+
 def post_list(request):
     posts=Post.objects.order_by('published_date')
     # posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return  render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
 
 def post_detail(request, pk):
     post=get_object_or_404(Post, pk=pk)
@@ -27,6 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -40,3 +43,4 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
